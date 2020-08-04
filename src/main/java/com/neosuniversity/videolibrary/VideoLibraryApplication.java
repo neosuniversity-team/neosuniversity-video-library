e5082f9cd984a783837c0eb1c097d6d25b9304ca
neossoftware <mario.hidalgo@yahoo.com.mx>
2020-08-04 13:32:36 -0500

    baseline init

--- src/main/java/com/neosuniversity/videolibrary/VideoLibraryApplication.java
+++ src/main/java/com/neosuniversity/videolibrary/VideoLibraryApplication.java
@@ -1,5 +1,6 @@
 package com.neosuniversity.videolibrary;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VideoLibraryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VideoLibraryApplication.class, args);
+	SpringApplication.run(VideoLibraryApplication.class, args);
+	
+	
+	
 	}
 
 }
