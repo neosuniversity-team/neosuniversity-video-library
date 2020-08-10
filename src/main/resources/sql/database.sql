72d73723c2433e558a03406b7fae588ee36a7b3d
neossoftware <mario.hidalgo@yahoo.com.mx>
2020-08-09 19:35:25 -0500

    sql

---
+++ src/main/resources/sql/database.sql
@@ -0,0 +1,17 @@
+
+/*
+  Ejecutar el create database unicamente si no ha creado la bd con mysql workbench
+*/
+CREATE DATABASE videodb CHARACTER SET utf8 COLLATE utf8_spanish_ci;
+
+/*
+  Crear el usuario neosvideo, unicamente si no ha creado el usuario en mysql workbench
+
+*/
+
+create user 'neosvideo'@'localhost' identified by 'neosvideo';
+
+
+GRANT ALL PRIVILEGES ON videodb.* TO 'neossoftware'@'localhost' WITH GRANT OPTION;
+
+FLUSH PRIVILEGES;
