97b62d6ef0b84b0e02130e822ad6b8842edcdfe6
neossoftware <mario.hidalgo@yahoo.com.mx>
2020-08-10 16:53:23 -0500

    baseline

---
+++ src/main/java/com/neosuniversity/videolibrary/controllers/MainController.java
@@ -0,0 +1,17 @@
+package com.neosuniversity.videolibrary.controllers;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/login")
+	public String hello() {
+		
+		return "login";
+		
+	}
+
+}
