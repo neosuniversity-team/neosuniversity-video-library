ff1d9af28bba3845bbddb43f721afb46474dc0d8
hhugohm <hhugohm@gmail.com>
2020-12-16 15:56:03 -0600

    update code

--- src/main/java/com/neosuniversity/videolibrary/entities/Role.java
+++ src/main/java/com/neosuniversity/videolibrary/entities/Role.java
@@ -1,7 +1,9 @@
 package com.neosuniversity.videolibrary.entities;
 
+import java.io.Serializable;
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -19,13 +21,20 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 @Entity
-@Table(name = "ROLE",schema="VIDEODB")
-public class Role {
+@Table(name = "ROLE_TABLE",schema="VIDEODB")
+public class Role implements Serializable{
 	
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="ID_ROLE",nullable=false)
+    private Long idRole;
+
+	@Column(name="NAME_ROLE",length=100,nullable=false)
     private String name;
 
     @ManyToMany(mappedBy = "roles")
